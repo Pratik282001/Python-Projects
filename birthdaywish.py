b0cca726b37245863bf838e6af3e07562e132d36
Pratik282001 <54789620+Pratik282001@users.noreply.github.com>
2020-08-26 17:05:55 +0530

    remaining send mail

--- birthdaywish.py
+++ birthdaywish.py
@@ -1,8 +1,25 @@
 import pandas as pd
+import datetime
 
-def sendmail():
-    pass
+def sendmail(to, sub, msg):
+    print(f'Sending Email to {to}')
+
 
 if __name__ == "__main__":
-    df = pd.read_excel('data.xlsx')
-    print()
+    df = pd.read_excel('Book1.xlsx')
+    print("----------Birthday Database-------------")
+    print(df) 
+    today = datetime.datetime.now().strftime("%d-%m")    
+    YearNow = datetime.datetime.now().strftime("%y")    
+    # print(today) prints today's date
+    writeInd=[]
+    for index, item in df.iterrows():
+        #print(index,item['birthday'])
+        bday = item['birthday'].strftime("%d-%m")
+        if(today==bday) and YearNow not in str(item['year']) :
+            sendmail(item['email'], "Happy Birthday", item['dialog'])
+            writeInd.append(index)
+    print(writeInd)
+    for i in writeInd:
+        yr= df.loc[i, 'year']
+        print(yr)
